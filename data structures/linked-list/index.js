e019f4440933adcd8a3e2d632e32a2a9fa4024d0
darya <shashko.darya@gmail.com>
2018-12-10 09:27:40 +0300

    singlyLinked list push method

---
+++ data structures/linked-list/index.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(val) {
+        this.element = val;
+        this.head = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+const list = new SinglyLinkedList();
+list.push('1');
+list.push('2');
+list.push('K');
+console.log(list.head);
+console.log(list.tail);
+console.log(list.head.next);
+console.log(list.head.next.next);
+console.log(list.head.next.next);
