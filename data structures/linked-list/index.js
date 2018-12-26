90c47524889b0dcfa286a30ccd7f556a8f3d9ee4
darya <shashko.darya@gmail.com>
2018-12-26 22:54:16 +0300

    queue methods

--- data structures/linked-list/index.js
+++ data structures/linked-list/index.js
@@ -9,7 +9,7 @@ class SinglyLinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
-        this.length = 0;
+        this.lgitength = 0;
     }
 
     push(val) {
---
+++ data structures/queue/index.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val) {
+        const newNode = new Node(val);
+        if (this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.size++;
+    }
+
+    dequeue() {
+        if (this.size === 0) return undefined;
+        if (this.first === this.last) {
+            this.last = null;
+        } else {
+         this.first = this.first.next;
+        }
+        this.size--;
+        return current.value;
+    }
+}
